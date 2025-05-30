24d5d42754044062991285b6464375d18b611d25
Sheape <86521166+Sheape@users.noreply.github.com>
2025-05-30 12:36:28 +0800

    day 2(part 1): red-nosed reports

---
+++ src/day_2.rs
@@ -0,0 +1,61 @@
+use std::{fs, path::Path};
+
+use crate::Solution;
+
+pub struct Day2;
+
+fn check_line(line: &[u8]) -> bool {
+    if line.len() < 2 {
+        return true;
+    }
+
+    if line[0] == line[1] {
+        return false;
+    }
+
+    let is_increasing = line[0] < line[1];
+
+    for window in line.windows(2) {
+        let (first, second): (u8, u8) = (window[0], window[1]);
+
+        if (first < second) != is_increasing || first == second || first.abs_diff(second) > 3 {
+            return false;
+        }
+    }
+
+    true
+}
+
+impl Solution for Day2 {
+    fn part1() -> u32 {
+        let input = Path::new("input/day2.txt");
+        let contents = fs::read_to_string(input).expect("Something wrong with opening this file");
+        let safe_reports = contents
+            .lines()
+            .map(|line| {
+                line.split_ascii_whitespace()
+                    .map(|number| number.parse::<u8>().unwrap())
+                    .collect::<Vec<u8>>()
+            })
+            .filter(|line| check_line(line))
+            .count();
+
+        safe_reports.try_into().unwrap()
+    }
+
+    fn part2() -> u32 {
+        let input = Path::new("input/day2.txt");
+        let contents = fs::read_to_string(input).expect("Something wrong with opening this file");
+        let safe_reports = contents
+            .lines()
+            .map(|line| {
+                line.split_ascii_whitespace()
+                    .map(|number| number.parse::<u8>().unwrap())
+                    .collect::<Vec<u8>>()
+            })
+            .filter(|line| check_line(line))
+            .count();
+
+        safe_reports.try_into().unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod day_1;
+mod day_2;
 mod solution;
 
 use day_1::Day1;
+use day_2::Day2;
 pub use solution::Solution;
 
 fn main() {
@@ -9,6 +11,11 @@ fn main() {
     println!(
         "DAY 1 [PART 2]: The total similarity score is: {}",
         Day1::part2()
+    );
+
+    println!(
+        "DAY 2 [PART 1]: The number of safe reports is: {}",
+        Day2::part1()
     );
 }
 
@@ -24,5 +31,10 @@ mod test {
     #[test]
     fn test_day_1_part_2() {
         assert_eq!(Day1::part2(), 23741109)
+    }
+
+    #[test]
+    fn test_day_2_part_1() {
+        assert_eq!(Day2::part1(), 314)
     }
 }
